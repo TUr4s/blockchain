b190ded19fb4680914082c4654d44424c71259c0
TUr4s <urasawa-takamasa037@g.ecc.u-tokyo.ac.jp>
2024-02-02 17:20:12 +0900

    a lecture about blockchain

---
+++ scripts/execute.js
@@ -0,0 +1,26 @@
+const { ethers, network } = require("hardhat");
+async function main() {
+// 既にデプロイされたLockコントラクトのアドレス
+const deployedLockAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+// 使用するアカウントのアドレス
+const userAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+// Lockコントラクトのインターフェースを取得
+const Lock = await ethers.getContractFactory("Lock");
+// 既存のコントラクトのインスタンスを取得
+const lock = await Lock.attach(deployedLockAddress);
+// 指定したアドレスのSignerを取得
+const signer = await ethers.getSigner(userAddress);
+// 指定したアドレスでwithdraw関数を実行
+try {
+console.log("Attempting to withdraw with specified address...");
+await lock.connect(signer).withdraw();
+console.log("Withdrawal successful.");
+} catch (error) {
+console.error("Failed to withdraw:", error.message);
+}
+}
+main().then(() => process.exit(0))
+.catch((error) => {
+console.error(error);
+process.exit(1);
+});
